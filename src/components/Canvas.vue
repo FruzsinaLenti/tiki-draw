13927d646ace6e77a45def44ce4678e697631655
Fruzsina Lenti <fruzsina.lenti@digitalnatives.hu>
2020-05-25 13:35:39 +0200

    feat: canvas resize handling, drawing function works properly

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <canvas
-      width="800"
-      height="800"
       ref="canvas"
-      v-once
       @mousemove="draw"
       @mousedown="startDrawing"
       @mouseup="stopDrawing"
       @mouseout="stopDrawing"
+      @touchmove="draw"
+      @touchstart="startDrawing"
+      @touchend="stopDrawing"
+      @touchcancel="stopDrawing"
     ></canvas>
   </div>
 </template>
@@ -20,9 +21,10 @@ export default {
       canvas: null,
       ctx: null,
       isDrawing: false,
-      lastX: 0,
-      lastY: 0,
-      hue: 0
+      lastX: null,
+      lastY: null,
+      hue: 0,
+      randomNumber: Math.random()
     };
   },
 
@@ -32,20 +34,49 @@ export default {
     },
     stopDrawing() {
       this.isDrawing = false;
+      this.lastX = null;
+      this.lastY = null;
+    },
+    handleResize() {
+      this.canvas.height = window.innerHeight;
+      this.canvas.width = window.innerWidth;
+      this.loadImage()
+        .then(img => {
+          this.ctx.drawImage(
+            img,
+            window.innerWidth / 2 - 150,
+            window.innerHeight / 2 - 150
+          );
+        })
+        .catch(err => console.error(err));
     },
     draw(e) {
       if (!this.isDrawing) return;
 
       this.ctx.lineJoin = "round";
-      this.ctx.lineWidth = 1;
+      this.ctx.lineWidth = 6;
       this.ctx.lineCap = "round";
-
       this.ctx.strokeStyle = `hsl(${this.hue}, 100%, 50%)`;
-      this.ctx.lineTo(e.offsetX, e.offsetY);
-      this.ctx.stroke();
+
       this.ctx.beginPath();
-      //this.ctx.moveTo(this.lastX, this.lastY); //this is awesome
-      this.ctx.moveTo(e.offsetX, e.offsetY);
+      let lineToX;
+      let lineToY;
+      if (e.type === "touchmove") {
+        lineToX = e.touches[0].clientX;
+        lineToY = e.touches[0].clientY;
+      } else {
+        lineToX = e.offsetX;
+        lineToY = e.offsetY;
+      }
+      this.ctx.moveTo(this.lastX || lineToX, this.lastY || lineToY);
+      this.ctx.lineTo(lineToX, lineToY);
+
+      this.ctx.stroke();
+      if (e.type === "touchmove") {
+        [this.lastX, this.lastY] = [e.touches[0].clientX, e.touches[0].clientY];
+      } else {
+        [this.lastX, this.lastY] = [e.offsetX, e.offsetY];
+      }
 
       this.hue++;
       if (this.hue >= 360) {
@@ -54,37 +85,38 @@ export default {
     },
 
     loadImage() {
-      const img = new Image();
-      const randomNumber = Math.random();
+      return new Promise((resolve, reject) => {
+        const img = new Image();
 
-      console.log(
-        `https://api.adorable.io/avatars/${window.innerHeight}/${randomNumber}`
-      );
-
-      img.src = `https://api.adorable.io/avatars/300/${randomNumber}`;
-      img.onload = () => {
-        this.ctx.drawImage(
-          img,
-          window.innerWidth / 2 - 150,
-          window.innerHeight / 2 - 150
-        );
-        let pixels = this.ctx.getImageData(100, 100, 1, 1);
-
-        this.ctx.putImageData(pixels, 150, 10);
-        console.log(pixels, "pp");
-      };
-
-      // const faceColor =  window.innerWidth/2 - 152
+        img.addEventListener("load", () => resolve(img));
+        img.addEventListener("error", err => reject(err));
+        img.src = `https://api.adorable.io/avatars/300/${this.randomNumber}`;
+      });
     }
   },
 
   mounted() {
-    window.addEventListener("DOMContentLoaded", this.loadImage);
     this.canvas = this.$refs.canvas;
     this.ctx = this.canvas.getContext("2d");
 
     this.canvas.height = window.innerHeight;
     this.canvas.width = window.innerWidth;
+
+    this.loadImage()
+      .then(img => {
+        this.ctx.drawImage(
+          img,
+          window.innerWidth / 2 - 150,
+          window.innerHeight / 2 - 150
+        );
+      })
+      .catch(err => console.error(err));
+
+    window.addEventListener("resize", this.handleResize);
+  },
+
+  beforeDestroy() {
+    window.removeEventListener("resize", this.handleResize);
   }
 };
 </script>
